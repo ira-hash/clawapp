210431573a34074a62460f1f33b39fc5c82371d3
Ira Kim <ira@gemmini.io>
2026-02-02 17:36:35 +0000

    chore: 컴포넌트 인덱스 파일 추가

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+// Core components
+export { TabBar, type TabName } from './TabBar';
+export { AgentListItem } from './AgentListItem';
+export { NetworkBanner } from './NetworkBanner';
+export { ErrorBoundary } from './ErrorBoundary';
+export { LoadingSpinner } from './LoadingSpinner';
+export { ToastProvider, useToast } from './Toast';
+
+// Re-export chat components
+export * from './chat';
